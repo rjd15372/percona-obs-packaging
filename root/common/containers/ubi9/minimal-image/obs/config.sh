efa390e95e92a4949a70686299e764d4c69830a0
Ricardo Dias <ricardo.dias@percona.com>
2026-05-25 12:11:17 +0100

    Improve base percona-ubi9-minimal container image size
    Signed-off-by: Ricardo Dias <ricardo.dias@percona.com>

--- root/common/containers/ubi9/minimal-image/obs/config.sh
+++ root/common/containers/ubi9/minimal-image/obs/config.sh
@@ -7,6 +7,9 @@ test -f /.profile && . /.profile
 
 echo "Configure UBI image: [$kiwi_iname]..."
 
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta
+
 microdnf clean all || true
 
 rm -rf {/target,}/usr/share/doc
@@ -14,10 +17,14 @@ rm -rf {/target,}/usr/share/man
 rm -rf {/target,}/usr/share/info
 rm -rf {/target,}/usr/share/locale/*
 
-rm -rf {/target,}/var/log/{alternatives.log,lastlog,tallylog,zypper.log,zypp/history,YaST2}; \
-    rm -rf {/target,}/run/*; \
-    rm -f {/target,}/etc/{shadow-,group-,passwd-,.pwd.lock}; \
-    rm -f {/target,}/usr/lib/sysimage/rpm/.rpm.lock; \
-    rm -f {/target,}/var/lib/zypp/AnonymousUniqueId; \
-    rm -f {/target,}/var/lib/zypp/AutoInstalled; \
-    rm -f {/target,}/var/cache/ldconfig/aux-cache
+rm -rf {/target,}/var/log/{alternatives.log,lastlog,tallylog}
+rm -rf {/target,}/run/*
+rm -f {/target,}/etc/{shadow-,group-,passwd-,.pwd.lock}
+rm -f {/target,}/usr/lib/sysimage/rpm/.rpm.lock
+rm -f {/target,}/var/cache/ldconfig/aux-cache
+
+rm -rf {/target,}/usr/share/zoneinfo
+rm -rf {/target,}/usr/share/gnupg
+rm -rf {/target,}/usr/share/icons
+rm -f {/target,}/var/lib/rpm/__db.*
+rm -f {/target,}/var/log/dnf*.log {/target,}/var/log/hawkey.log
